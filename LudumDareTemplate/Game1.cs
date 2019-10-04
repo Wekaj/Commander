bfb63f5d4b62ba5efc005107c98bdad93a957ed2
Wekaj <wekkaj@gmail.com>
2019-10-04 13:33:11 +1300

    Add project files.

---
+++ LudumDareTemplate/Game1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LudumDareTemplate {
+    public class Game1 : Game {
+        private readonly GraphicsDeviceManager _graphics;
+
+        private SpriteBatch _spriteBatch;
+
+        public Game1() {
+            _graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize() {
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent() {
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.TransparentBlack);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ LudumDareTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LudumDareTemplate {
+    public static class Program {
+        [STAThread]
+        private static void Main() {
+            using (var game = new Game1()) {
+                game.Run();
+            }
+        }
+    }
+}
